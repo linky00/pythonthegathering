0e03d3bbeb59d20d05d5a574665f1d932329b80f
linky00 <linky00@plotholestudios.com>
2017-10-07 19:47:12 +0100

    this is the last time i'm fixing this

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pythonthegathering',
     packages=['pythonthegathering'],
-    version='1.2',
+    version='1.2.1',
     description='Replaces everything good and practical about Python with MTG!',
     author='Theo Hamilton/linky00',
     author_email='linky00@plotholestudios.com',
     url='https://github.com/linky00/pythonthegathering',
-    download_url='https://github.com/linky00/pythonthegathering/archive/v1.2.tar.gz',
+    download_url='https://github.com/linky00/pythonthegathering/archive/v1.2.1.tar.gz',
     keywords='decorators mtg',
     classifiers=[
         'Development Status :: 3 - Alpha',
