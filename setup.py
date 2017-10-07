47036a4bf8ea0b6cd7934381dabc5e9bf3f44765
linky00 <linky00@plotholestudios.com>
2017-10-07 17:22:56 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Theo Hamilton/linky00',
     author_email='linky00@plotholestudios.com',
     url='https://github.com/linky00/pythonthegathering',
-    download_url='https://github.com/linky00/pythonthegathering/archive/v1.1.tar.gz',
+    download_url='https://github.com/linky00/pythonthegathering/archive/v1.2.tar.gz',
     keywords='decorators mtg',
     classifiers=[
         'Development Status :: 3 - Alpha',
